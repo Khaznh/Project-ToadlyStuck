057830a0bb98209328954f04a1bbcb67f697b18b
Khaznh <anhktsnamkhanhcs3@gmail.com>
2026-08-01 13:46:04 +0700

    Feat: add level 18

---
+++ Assets/_Scripts/Level/Info/LV18Info.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class LV18Info : LVInfo
+{
+    public void OnEnable()
+    {
+        PlayerPrefs.SetInt("LastLevelPlayed", 1);
+    }
+
+    public void OnDisable()
+    {
+        LevelUnlockManager.Instance.UnlockNextLevel(19);
+    }
+
+    public void OnPlayerSpike(Collider2D collision, Activer activer)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+        PlayerController.Instance.playerDeath.Die();
+        PlayerController.Instance.transform.position = playerSpawn.position;
+    }
+}
