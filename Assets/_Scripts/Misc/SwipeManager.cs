2094b6c6c08ae174c8a6cbf1d15aedc966e35e0f
Khaznh <anhktsnamkhanhcs3@gmail.com>
2026-08-02 10:06:27 +0700

    Feat: add level 22

--- Assets/_Scripts/Misc/SwipeManager.cs
+++ Assets/_Scripts/Misc/SwipeManager.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SwipeManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+public class SwipeManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     public Action OnSwipeLeft;
 
